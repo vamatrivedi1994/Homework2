67dbbb425997e838f13df3b8430d70dd7a31c18b
Vama Trivedi <vama.trivedi1994@gmail.com>
2018-07-11 23:37:18 -0700

    First commit

---
+++ eslintrc22.js
@@ -0,0 +1,24 @@
+module.exports = {
+"env": {
+"browser": true
+},
+"extends": "eslint:recommended",
+"rules": {
+"indent": [
+"error",
+2
+],
+"linebreak-style": [
+"error",
+"windows" // change to “unix” if on Mac OSX or on Linux
+],
+"quotes": [
+"error",
+"double"
+],
+"semi": [
+"error",
+"always"
+]
+}
+};
